8a6ffb343bcdaf46c014ad54732e8cf58b95a36a
Acie <33143065+acierate@users.noreply.github.com>
2021-05-10 22:02:52 -0500

    Calculator
    Fix'd

--- Calculator.java
+++ Calculator.java
@@ -15,64 +15,43 @@ public class Calculator {
 	 
 	 Values Calculation = new Values();
 	 Scanner Scan = new Scanner(System.in);
-	 
-	 
-			 System.out.println("Enter number:");
-			 Calculation.x = Scan.nextDouble();
-			 
-	 System.out.println("Enter operator (+,-,*,/)");
-	 Calculation.operator = Scan.next().charAt(0);
-	 
-			System.out.println("Enter number:");
-			
-			
-			 Calculation.y = Scan.nextDouble();
-			 
-			 
-			 
-			 
-			 
-		switch (Calculation.operator) {
-		case '+': Calculation.z = Calculation.x + Calculation.y;
-				break;
-		case '*': Calculation.z = Calculation.x * Calculation.y;
-				break;
-		case '-': Calculation.z = Calculation.x - Calculation.y;
-				break;
-		case '/': Calculation.z = Calculation.x / Calculation.y;
-				break;
-		}
-	 
+
+		boolean work = true;
+		do {	
+		
+		 
+		 
+		 System.out.println("Enter number:");
+		 Calculation.x = Scan.nextDouble();
+		 
+ System.out.println("Enter operator (+,-,*,/)");
+ Calculation.operator = Scan.next().charAt(0);
+ 
+		System.out.println("Enter number:");
+		 Calculation.y = Scan.nextDouble();
+		 
+		 
+		 
+		 
+		 
+		 switch (Calculation.operator) {
+		 
+		 case '+': Calculation.z = Calculation.x + Calculation.y;
+		 break;
+		 case '*': Calculation.z = Calculation.x * Calculation.y;
+		 break;
+		 case '-': Calculation.z = Calculation.x - Calculation.y;
+		 break;
+		 case '/': Calculation.z = Calculation.x / Calculation.y;
+		 break;
+	}
 	
-		
-		System.out.println(Calculation.x+" " +Calculation.operator+" "+Calculation.y+" = "+Calculation.z );
-		
-		do {
-			
-			
-		System.out.println(" Enter (+,-,*,/)");
-		Calculation.operator2 = Scan.next().charAt(0);
-		
-		
-		System.out.println(" Enter number");
-		Calculation.x = Scan.nextDouble();
-		
-		switch (Calculation.operator2) {
-		case '+': Calculation.answer = Calculation.x + Calculation.z;
-				break;
-		case '*': Calculation.answer = Calculation.x * Calculation.z;
-				break;
-		case '-': Calculation.answer = Calculation.x - Calculation.z;
-				break;
-		case '/': Calculation.answer = Calculation.x / Calculation.z;
-				break;
+	
+	System.out.println(Calculation.x+" " +Calculation.operator+" "+Calculation.y+" = "+Calculation.z );
+	
+	
 		}
 		
-		System.out.println(Calculation.z+" " +Calculation.operator2+" "+Calculation.x+" = "+Calculation.answer );
-		
-		Calculation.z = Calculation.answer;
-		}
-		
-		while (Calculation.answer < Double.MAX_VALUE );
+		while (work == true);
  }
 }
